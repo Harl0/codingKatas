3bdf592a2cb6ff3f58419ca44c33f2eea91e0b20
Jason Harris <jiharris90@gmail.com>
2016-07-19 20:03:26 +0100

    Added Maven executable jar functionality and Tutorial11

--- java/Tutorial1/src/jason/Application.java
+++ java/Tutorial1/src/jason/Application.java
@@ -1,10 +1,11 @@
+package jason;
 
 public class Application {
 
 	public static void main(String[] args) {
 		int myNumber = 5;
 		
-		System.out.println("The answer is " + myNumber);
+		System.out.println("The answer will be " + myNumber);
 	}
 
 }
---
+++ java/Tutorial11_ArraysCont/src/com/jason/application/Array.java
@@ -0,0 +1,16 @@
+package com.jason.application;
+
+public class Array {
+
+	public static void main(String args[]) {
+
+		String[] fruits = { "apple", "banana", "orange" };
+
+		for (String fruit : fruits) {
+			System.out.println(fruit);
+
+		}
+
+	}
+
+}
